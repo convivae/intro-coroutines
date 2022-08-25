f3cc5b6836e51fa71b72be2ccb09988b63b772fe
convivae <l16271012@gmail.com>
2022-08-25 14:13:37 +0800

    complete request3: callback

--- src/tasks/Request3Callbacks.kt
+++ src/tasks/Request3Callbacks.kt
@@ -5,22 +5,46 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.util.*
-import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.CountDownLatch
+
+// solution: https://play.kotlinlang.org/hands-on/Introduction%20to%20Coroutines%20and%20Channels/03_UsingCallbacks
 
 fun loadContributorsCallbacks(service: GitHubService, req: RequestData, updateResults: (List<User>) -> Unit) {
     service.getOrgReposCall(req.org).onResponse { responseRepos ->
         logRepos(req, responseRepos)
         val repos = responseRepos.bodyList()
-        val allUsers = mutableListOf<User>()
+        val allUsers = Collections.synchronizedList(mutableListOf<User>())
+
+        // 方法 1：使用 AtomicInteger
+//        val numberOfProcessed = AtomicInteger()
+//        for (repo in repos) {
+//            service.getRepoContributorsCall(req.org, repo.name).onResponse { responseUsers ->
+//                logUsers(repo, responseUsers)
+//                val users = responseUsers.bodyList()
+//                allUsers += users
+//                if(numberOfProcessed.incrementAndGet() == repos.size){
+//                    updateResults(allUsers.aggregate())
+//                }
+//            }
+//        }
+
+        // 方法 2：使用 CountDownLatch
+        // 每次调用后对 counter 减 1 直至为 0
+        val countDownLatch = CountDownLatch(repos.size)
         for (repo in repos) {
             service.getRepoContributorsCall(req.org, repo.name).onResponse { responseUsers ->
                 logUsers(repo, responseUsers)
                 val users = responseUsers.bodyList()
                 allUsers += users
+                countDownLatch.countDown()
             }
         }
-        // TODO: Why this code doesn't work? How to fix that?
+        countDownLatch.await()
         updateResults(allUsers.aggregate())
+
+        // TODO: Why this code doesn't work? How to fix that?
+        // 因为调用下面这句话的时候数据还未返回，allUsers 是空的
+        // updateResults(allUsers.aggregate())
     }
 }
 
